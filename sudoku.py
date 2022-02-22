2372cd28c9a6356b52ee55a24f5f9f27d97c7f4d
Faisal <faisal@Faisals-MacBook-Pro.local>
2022-02-21 16:07:18 -0800

    added the opencv commands to use the webcam to take the image and change it to grayscale and apply thresholds to get a better representation of the image

--- sudoku.py
+++ sudoku.py
@@ -22,7 +22,56 @@ def play():
     flag = 0
     solution_found = False
 
-    
+    while True:
+
+        ret, frame = cap.read()
+
+        gray_scale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        gray_scale = cv2.GaussianBlur(gray_scale, (7, 7), 0)
+        thresh = cv2.adaptiveThreshold(
+            gray_scale, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 9, 2)
+
+        contours, hierarchy = cv2.findContours(
+            thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        contour_size = None
+        maxArea = 0
+
+        for contour in contours:
+            area = cv2.contourArea(contour)
+            if area > 25000:
+                arc_length = cv2.arcLength(contour, True)
+                polygone = cv2.approxPolyDP(contour, 0.01 * arc_length, True)
+                if area > maxArea and len(polygone) == 4:
+                    contour_size = polygone
+                    maxArea = area
+
+        if contour_size is not None:
+            cv2.drawContours(frame, [contour_size], 0, (0, 255, 0), 2)
+            coordinates = np.vstack(contour_size).squeeze()
+            coordinates = sorted(coordinates, key=operator.itemgetter(1))
+
+            if coordinates[0][0] < coordinates[1][0]:
+                if coordinates[3][0] < coordinates[2][0]:
+                    coord1 = np.float32([coordinates[0], coordinates[1], coordinates[3], coordinates[2]])
+                else:
+                    coord1 = np.float32([coordinates[0], coordinates[1], coordinates[2], coordinates[3]])
+            else:
+                if coordinates[3][0] < coordinates[2][0]:
+                    coord1 = np.float32([coordinates[1], coordinates[0], coordinates[3], coordinates[2]])
+                else:
+                    coord1 = np.float32([coordinates[1], coordinates[0], coordinates[2], coordinates[3]])
+            coord2 = np.float32([[0, 0], [grid_size, 0], [0, grid_size], [
+                              grid_size, grid_size]])
+            M = cv2.getPerspectiveTransform(coord1, coord2)
+            # is the original sudoku without the soluution
+            gray_scale_extracted_points = cv2.warpPerspective(frame, M, (grid_size, grid_size))
+            gray_scale_extracted_points = cv2.cvtColor(gray_scale_extracted_points, cv2.COLOR_BGR2GRAY)
+            gray_scale_extracted_points = cv2.adaptiveThreshold(
+                gray_scale_extracted_points, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 7, 3)
+
+            cv2.imshow("Extracted #s", gray_scale_extracted_points)
+
+          
 
 
     # out.release()
